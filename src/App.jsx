7a24758f2124ccc5d3d6c1e422abcb07d08d913d
ILTD <heinhtet9512@gmail.com>
2024-09-05 21:56:42 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  
+    {
+      path: "/", 
+      element: <Home/>,
+    },
+    {
+      path: "/login", 
+      element: <Login/>,
+    },
+    {
+      path: "/signup", 
+      element: <Signup/>,
+    }
+  
+])
+
+function App() {
+  
+  return (
+    <>
+      <RouterProvider router={router}/>
+    </>
+  
+  )
+}
+
+export default App
