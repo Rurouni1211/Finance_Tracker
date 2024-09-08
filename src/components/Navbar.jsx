e8d1d75b14adfb1fd8026a5b3bd919c7d668f3a7
ILTD <heinhtet9512@gmail.com>
2024-09-08 21:48:06 +0900

    page refresh doesn't effect login

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
 import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 export default function Navbar(){
     const {logout} = useLogout()
+    const {user} = useAuthContext()
     console.log('Navbar rendering');
     return (
         <div className="fixed top-0 left-0 w-full flex justify-between items-center p-4 bg-gray-800 z-50">
@@ -14,9 +16,22 @@ export default function Navbar(){
         
         {/* Right Side - Login and Signup */}
         <div className="flex space-x-4 xl:mr-28">
-            <Link to="/login" className="bg-transparent text-white hover:text-gray-400">Login</Link>
-            <Link to="/signup" className="bg-transparent text-white hover:text-gray-400">Signup</Link>
-            <button className="bg-transparent text-white hover:text-gray-400" onClick={logout}>Logout</button>
+            {!user && (
+                <>
+                    <Link to="/login" className="bg-transparent text-white hover:text-gray-400">Login</Link>
+                    <Link to="/signup" className="bg-transparent text-white hover:text-gray-400">Signup</Link>
+                </>
+            )}
+           
+           {user && (
+            <>
+             <span className="text-white pt-3">hello, {user.displayName}</span>
+             <button className="bg-transparent text-white hover:text-gray-400" onClick={logout}>Logout</button>
+            </>
+               
+                
+            )}
+            
         </div>
     </div>
     );
